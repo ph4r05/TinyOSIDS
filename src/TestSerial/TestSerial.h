23fdf8060bac0d9218d868a1611c923dd49f39f6
ph4r05 <ph4r05@gmail.com>
2012-04-27 08:10:41 +0200

    test serial - working

---
+++ src/TestSerial/TestSerial.h
@@ -0,0 +1,14 @@
+
+#ifndef TEST_SERIAL_H
+#define TEST_SERIAL_H
+
+typedef nx_struct test_serial_msg {
+  nx_uint16_t counter;
+  nx_uint16_t received;
+} test_serial_msg_t;
+
+enum {
+  AM_TEST_SERIAL_MSG = 0x89,
+};
+
+#endif
